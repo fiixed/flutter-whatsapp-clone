d03647c11783ae60636f419d0d499c7b77a602ff
Andrew Bell <fiixed@gmail.com>
2020-01-14 17:16:56 -0600

    basic scaffolding

---
+++ lib/whatsapp_home.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+import 'pages/call_screen.dart';
+import 'pages/camera_screen.dart';
+import 'pages/chat_screen.dart';
+import 'pages/status_screen.dart';
+
+class WhatsAppHome extends StatefulWidget {
+  WhatsAppHome({Key key}) : super(key: key);
+
+  @override
+  _WhatsAppHomeState createState() => _WhatsAppHomeState();
+}
+
+class _WhatsAppHomeState extends State<WhatsAppHome>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      vsync: this,
+      initialIndex: 1,
+      length: 4,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WhatsApp'),
+        elevation: 0.7,
+        bottom: TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          tabs: <Widget>[
+            Tab(
+              icon: Icon(Icons.camera_alt),
+            ),
+            Tab(
+              text: 'CHATS',
+            ),
+            Tab(
+              text: 'STATUS',
+            ),
+            Tab(
+              text: 'CALLS',
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          CameraScreen(),
+          ChatScreen(),
+          StatusScreen(),
+          CallScreen(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).accentColor,
+        child: Icon(Icons.message), 
+        onPressed: () => print('Open Chats'),
+      ),
+    );
+  }
+}
